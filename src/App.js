95811696be56123bd0758a351d6ec993dc1e97f2
josedomingos919 <josedomingos919@gmail.com>
2021-09-08 16:25:54 +0100

    final list

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
+import { useState } from "react";
 import { myFiles } from "./assets";
 import "./styles/main.css";
-import { Button } from "reactstrap";
-import { getNotItemInList } from "./util";
+import { getNotItemInList, removeAcento } from "./util";
 import ListContainer from "./components/list";
+import Select from "react-select";
+import dataBase from "./data/marcas.json";
 
 function App() {
   const { logo } = myFiles;
+  const options = dataBase;
+  const [itemName, setItemName] = useState("");
+
+  const getData = () => {
+    return dataBase.filter(({ label }) => {
+      const refacr = (text = "") =>
+        itemName.length <= label.length
+          ? removeAcento(
+              text.substring(0, itemName?.length)
+            ).toLocaleLowerCase()
+          : text;
+
+      return refacr(label) === refacr(itemName) && itemName.length;
+    });
+  };
 
   return (
     <div className="App">
@@ -13,8 +30,25 @@ function App() {
         <div>
           <img className="logo-img" src={logo} alt="logo" />
         </div>
+        <div className="select-container">
+          <Select
+            options={options}
+            onInputChange={(e) => {
+              if (e) setItemName(e);
+            }}
+            onChange={(e) => {
+              if (e?.label) setItemName(e?.label || "");
+            }}
+          />
+        </div>
       </header>
-      <main className="main-container">{<ListContainer />}</main>
+      <main className="main-container">
+        {getData().length ? (
+          <ListContainer data={getData()} />
+        ) : (
+          getNotItemInList()
+        )}
+      </main>
     </div>
   );
 }
--- src/components/list/index.js
+++ src/components/list/index.js
@@ -1,38 +1,88 @@
-import React from "react";
+import { useState } from "react";
 import { myFiles } from "../../assets";
+import { Modal, ModalFooter, ModalBody, Button } from "reactstrap";
 
-export default function ListContainer(props) {
+export default function ListContainer({ data = [] }) {
+  const [activeItem, setActiveItem] = useState([]);
+
+  const toggle = () => setModal(!modal);
+  const [modal, setModal] = useState(false);
+
   return (
     <div className="container-card-div">
+      <Modal isOpen={modal} toggle={() => {}}>
+        <ModalBody>
+          <div className="modal-container-in">
+            <p>Tem certeza que deseja ir para a tela?</p>
+          </div>
+        </ModalBody>
+        <ModalFooter>
+          <Button color="primary" onClick={toggle}>
+            Não
+          </Button>
+          <Button color="secondary" onClick={toggle}>
+            Sim
+          </Button>
+        </ModalFooter>
+      </Modal>
+
       <div>
         <h1>Resultado da Busca</h1>
       </div>
 
       <div className="container-list-div">
         <ul>
-          <li>
-            <section>
-              <div className="info-div">
-                <span>Maind info</span>
-              </div>
-              <div>
-                <button className="btn-detail">
-                  Ver detalhes da ação <img src={myFiles.iconDown}></img>
-                </button>
-              </div>
-            </section>
-            <div className="container-li-body">
-              <div>
-                <p>
-                  ftugybukmdvdvPor enquanto é só! Volte mais tarde para
-                  visualizar novas notificaçõe dfshfikjdsjjs
-                </p>
-              </div>
-              <div>
-                <button className="btn-see-more">VER MAIS</button>
-              </div>
-            </div>
-          </li>
+          {data.map(({ label, value, description, img }, i) => (
+            <li key={i}>
+              <section>
+                <div className="info-div">
+                  <span>{label} </span>
+                </div>
+                <div>
+                  <button
+                    onClick={() => {
+                      if (activeItem.includes(value))
+                        setActiveItem((prev) => [
+                          ...prev.filter((e) => e !== value),
+                        ]);
+                      else setActiveItem((prev) => [...prev, value]);
+                    }}
+                    className="btn-detail"
+                  >
+                    Ver detalhes da ação{" "}
+                    <img
+                      src={
+                        activeItem.includes(value)
+                          ? myFiles.iconUp
+                          : myFiles.iconDown
+                      }
+                      alt="seta"
+                    ></img>
+                  </button>
+                </div>
+              </section>
+              {activeItem.includes(value) && (
+                <div className="container-li-body">
+                  <div>
+                    <p>
+                      <img
+                        src={img}
+                        className="img-info"
+                        alt="marca-imagem"
+                        align="left"
+                      />
+                      {description}
+                    </p>
+                  </div>
+                  <div>
+                    <button onClick={toggle} className="btn-see-more">
+                      VER MAIS
+                    </button>
+                  </div>
+                </div>
+              )}
+            </li>
+          ))}
         </ul>
       </div>
     </div>
